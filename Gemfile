8defa604a320adb9c0c57044fed37ce4b27c9cbb
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-10 20:28:57 +0100

    Completed initial set up of datamapper

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source "https://rubygems.org"
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
+
+group :test do
+gem 'capybara'
+
+gem 'rspec'
+
+end
---
+++ app/bookmarkmanager_web.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require './data_mapper_setup'
+
+class BookMarkManagerWeb < Sinatra::Base
+   get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
