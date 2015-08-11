56bcab0f811bef413b3fdb7eb7c73b5503e7c0a4
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-11 17:04:40 +0100

    Added test for no tag

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -10,7 +10,6 @@ feature 'Adding tags' do
     expect(link.tags.map(&:name)).to include('education')
   end
 
-
   scenario 'I can add multiple tags to a new link' do
     visit '/links/new'
     fill_in 'url', with: 'http://www.makersacademy.com/'
@@ -20,5 +19,16 @@ feature 'Adding tags' do
     click_button 'Create link'
     link = Link.first
     expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
+
+  scenario 'I can add no tags to a new link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tag', with: ''
+
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags).to be_empty
   end
 end
