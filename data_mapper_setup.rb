380661fb82bf84ddf18e58cfdc3ab7b76ae70ac8
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-11 12:47:13 +0100

    Added db url

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -1,9 +1,9 @@
 require 'data_mapper'
 
-
+env = ENV['RACK_ENV'] || 'development'
 
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, ENV["DATABASE_URL"] || 'postgres://localhost/bookmark_manager')
+DataMapper.setup(:default, ENV['postgres://mvwihxoqlohpjo:6L7icTjGTR8RlsfVaeho0jxzsn@ec2-107-22-249-138.compute-1.amazonaws.com:5432/d25j3hjqkncbkb' || "postgres://localhost/bookmark_manager_#{env}")
 
 require './app/models/link' # require each model individually - the path may vary depending on your file structure.
 
