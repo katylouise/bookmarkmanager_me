869afa3b42ede125e258819a7881d46bbec8c557
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-11 10:50:22 +0100

    Three tests passing - added page to add new links

--- app/bookmarkmanager_web.rb
+++ app/bookmarkmanager_web.rb
@@ -6,11 +6,19 @@ class BookMarkManagerWeb < Sinatra::Base
     'Hello BookMarkManager'
   end
 
-   get '/links' do
+  get '/links' do
     @links = Link.all
     erb :'links/index'
   end
 
+  post '/links' do
+    Link.create(url: params[:url], title: params[:title])
+    redirect to('/links')
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
