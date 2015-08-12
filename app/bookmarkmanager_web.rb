406d8d44d5e1820f178375ce907442dc40027e41
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-12 12:37:12 +0100

    Can now add user

--- app/bookmarkmanager_web.rb
+++ app/bookmarkmanager_web.rb
@@ -2,8 +2,13 @@ require 'sinatra/base'
 require './data_mapper_setup'
 
 class BookMarkManager < Sinatra::Base
-  get'/' do
-    'Hello BookMarkManager'
+  set :views, proc { File.join(root, 'views') }
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  get '/' do
+    erb :index
   end
 
   get '/links' do
@@ -33,7 +38,22 @@ class BookMarkManager < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
-  set :views, proc { File.join(root, 'views') }
+
 end
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 feature 'Adding tags' do
   scenario 'I can add a single tag to a new link' do
     visit '/links/new'
@@ -25,7 +27,6 @@ feature 'Adding tags' do
     visit '/links/new'
     fill_in 'url', with: 'http://www.makersacademy.com/'
     fill_in 'title', with: 'Makers Academy'
-    fill_in 'tag', with: ''
 
     click_button 'Create link'
     link = Link.first
