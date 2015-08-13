f6e717be961ac2f53ca48283f6f062210f4d0aa3
Rebecca Appleyard <rklappleyard@gmail.com>
2015-08-13 11:29:45 +0100

    Finished implementing user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,21 @@ class User
   end
 
   def self.authenticate(email, password)
-    User.first(email: email)
+    user = User.first(email: email)
+  # if this user exists and the password provided matches
+  # the one we have password_digest for, everything's fine
+  #
+  # The Password.new returns an object that overrides the ==
+  # method. Instead of comparing two passwords directly
+  # (which is impossible because we only have a one-way hash)
+  # the == method calculates the candidate password_digest from
+  # the password given and compares it to the password_digest
+  # it was initialised with.
+  # So, to recap: THIS IS NOT A STRING COMPARISON
+  if user && BCrypt::Password.new(user.password_digest) == password
+    user
+  else
+    nil
+  end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,11 @@
 describe User do
+  let(:user) { create(:user) }
   it 'authenticates when given a valid email address and password' do
-    user = create(:user)
     authenticated_user = User.authenticate(user.email, user.password)
     expect(authenticated_user).to eq user
+  end
+
+  it 'does not authenticate when given an incorrect password' do
+    expect(User.authenticate(user.email, 'wrong_password')).to be_nil
   end
 end
